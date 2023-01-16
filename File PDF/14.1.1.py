b515e958203e960ee847689493f87bc2847f26c3
Daniel <kronkowski.daniel@gmail.com>
2023-01-16 20:09:54 +0100

    Chapter 14.1

---
+++ File PDF/14.1.1.py
@@ -0,0 +1,19 @@
+from PyPDF2 import PdfFileReader
+from pathlib import Path
+
+pdf_path = (Path.home()/ 'python-basics-exercises-master' 
+/ 'ch14-interact-with-pdf-files' 
+/ 'practice_files'
+/ 'Pride_and_Prejudice.pdf')
+
+pdf_reader = PdfFileReader(str(pdf_path))
+output_file_path = Path.home() / 'Pride_and_Prejudice.txt'
+
+with output_file_path.open(mode='w') as output_file:
+    title = pdf_reader.documentInfo.title
+    num_pages = pdf_reader.getNumPages()
+    output_file.write(f"{title}\nNumber of pages:{num_pages}\n\n")
+
+    for page in pdf_reader.pages:
+        text = page.extractText()
+        output_file.write(text)
--- File PDF/14.1.py
+++ File PDF/14.1.py
@@ -1,2 +1,18 @@
 from PyPDF2 import PdfFileReader
 from pathlib import Path
+
+pdf_path = (Path.home()/ 'python-basics-exercises-master' 
+/ 'ch14-interact-with-pdf-files' 
+/ 'practice_files'
+/ 'Pride_and_Prejudice.pdf')
+
+pdf = PdfFileReader(str(pdf_path))
+
+
+#print(pdf.getNumPages())
+#print(pdf.documentInfo.title)
+
+first_page = pdf.getPage(0)
+
+for page in pdf.pages:
+    print(page.extract_text())
