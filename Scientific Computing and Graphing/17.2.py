9b2e207657e3af2eca76d31397cb9cd46e6982ef
Daniel <kronkowski.daniel@gmail.com>
2023-01-30 20:56:21 +0100

    Chapter 17.2 Bar Charts and Histograms

--- Scientific Computing and Graphing/17.2.py
+++ Scientific Computing and Graphing/17.2.py
@@ -1,7 +1,8 @@
 from matplotlib import pyplot as plt
 import numpy as np
+from numpy import random
 
-
+# Line Charts
 days = np.arange(0, 21)
 other_site, real_python = days, days ** 2
 
@@ -12,4 +13,19 @@ plt.xlabel('Days of reading')
 plt.ylabel('Amount of Python Learned')
 plt.title("Python Learned Reading Real Python vs Other Site")
 plt.legend(['Other Site', 'Real Python'])
+plt.show()
+
+# Bar Charts
+fruits = {
+    "apples": 10,
+    "oranges": 16,
+    "bananas": 9,
+    "pears": 4,
+}
+plt.bar(fruits.keys(), fruits.values())
+plt.show()
+
+#Histograms
+plt.hist(random.randn(10000),(20))
+plt.savefig('hist.png')
 plt.show()
